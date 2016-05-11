e657e05db34cbf8bec77a4cd16a04190d18f413a
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-11 10:23:50 +0700

    Add reducers test suite

---
+++ tests/reducers/index.js
@@ -0,0 +1,14 @@
+// tests/reducers/index.js
+// -----------------------
+//
+// @author  Natcha Luang - Aroonchai <me@nomkhonwaan.com>
+// @created May 10, 2016
+//
+
+import expect from 'expect'
+import reducers from '../../reducers'
+
+describe('reducers/index.js', () => {
+  it('should return an initialState', () => {
+  })
+})
