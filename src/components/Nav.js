5b4e830c50ea4eaab3aff9e195826691aff84d0f
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-14 21:44:33 +0700

    Enable FontAwesome Icon in Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,22 +12,7 @@ import { onClickMenuButton } from '../actions'
 
 const Nav = ({ dispatch, isExpanded }) => {
   const styles = require('../stylesheets/components/Nav.scss')
-  const menuItems = [{
-    title: 'Home',
-    href: 'https://www.nomkhonwaan.com'
-  }, {
-    title: 'GitHub',
-    href: 'https://github.com/nomkhonwaan'
-  }, {
-    title: 'Twitter',
-    href: 'https://twitter.com/nomkhonwaan'
-  }, {
-    title: 'LinkedIn',
-    href: 'https://linkedin.com/in/nomkhonwaan'
-  }, {
-    title: 'RSS',
-    href: 'https://www.nomkhonwaan.com/rss'
-  }]
+  const faStyles = require('font-awesome.css')
   
   return (
     <nav className={styles['layout-nav']}>
@@ -37,10 +22,50 @@ const Nav = ({ dispatch, isExpanded }) => {
         styles._unmargin, 
         { [styles.expanded]: isExpanded }
       )}>
-        {menuItems.map((item, key) => 
+        {[{
+          title: 'Home',
+          href: '//www.nomkhonwaan.com',
+          iconClass: [
+            faStyles.fa,
+            faStyles['fa-fw'],
+            faStyles['fa-home'] 
+          ]
+        }, {
+          title: 'GitHub',
+          href: '//github.com/nomkhonwaan',
+          iconClass: [
+            faStyles.fa,
+            faStyles['fa-fw'],
+            faStyles['fa-github-square']
+          ]
+        }, {
+          title: 'Twitter',
+          href: '//twitter.com/nomkhonwaan',
+          iconClass: [
+            faStyles.fa,
+            faStyles['fa-fw'],
+            faStyles['fa-twitter-square']
+          ]
+        }, {
+          title: 'LinkedIn',
+          href: '//linkedin.com/in/nomkhonwaan',
+          iconClass: [
+            faStyles.fa,
+            faStyles['fa-fw'],
+            faStyles['fa-linkedin-square']
+          ]
+        }, {
+          title: 'RSS',
+          href: '//www.nomkhonwaan.com/rss',
+          iconClass: [
+            faStyles.fa,
+            faStyles['fa-fw'],
+            faStyles['fa-rss-square']
+          ]
+        }].map((item, key) => 
           <li className={styles['menu-item']} key={key}>
             <a href={item.href}>
-              {item.title}
+              <i className={classNames(item.iconClass)}></i>&nbsp;{item.title}
             </a>
           </li>)}
       </ul>
