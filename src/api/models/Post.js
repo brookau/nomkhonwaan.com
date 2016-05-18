2dc8104951620551b00a89db6e9fceb5e6a34354
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-18 22:06:32 +0700

    Create Post schema

--- src/api/models/Post.js
+++ src/api/models/Post.js
@@ -5,8 +5,18 @@
 // @created May 14, 2016
 //
 
-import { model } from 'mongoose'
+import { model, Schema } from 'mongoose'
 
-// const Post = model('Post', )
+const Post = model('Post', {
+  createdAt:   { type: Date, default: Date.now },
+  updatedAt:   Date,
+  publishedAt: Date,
+  tags:        [Schema.Types.Mixed],
+  users:       [Schema.Types.Mixed],
+  title:       String,
+  slug:        String,
+  markdown:    String,
+  html:        String
+})
 
-// export default Post
+export default Post
