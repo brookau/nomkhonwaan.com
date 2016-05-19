2aa0fa27ee7d6460bc03ea1b69c9ede025fb8d0e
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-19 19:45:53 +0700

    Update user model

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -4,3 +4,14 @@
 // @author  Natcha Luang - Aroonchai <me@nomkhonwaan.com>
 // @created May 14, 2016
 //
+
+import { model, Schema } from 'mongoose'
+
+const User = model('User', {
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: Date,
+  email: String,
+  displayName: String
+})
+
+export default User
