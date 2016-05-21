db26f4ea6aca3646237745e41d9a050d03112fbb
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-21 16:08:25 +0700

    Add model exporter

---
+++ src/api/models/index.js
@@ -0,0 +1,14 @@
+// api/models/index.js
+// -------------------
+//
+// @author  Natcha Luang - Aroonchai <me@nomkhonwaan.com>
+// @created May 19, 2016
+//
+
+import Post from './Post'
+import User from './User'
+
+export default {
+  Post,
+  User
+}
