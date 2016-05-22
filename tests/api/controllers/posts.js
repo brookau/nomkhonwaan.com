6b6762adcd763760ed9deb1667df4b6d5ff5f3e1
Natcha Luang - Aroonchai <me@nomkhonwaan.com>
2016-05-22 21:37:24 +0700

    Create posts controller test with stub post model

--- tests/api/controllers/posts.js
+++ tests/api/controllers/posts.js
@@ -7,31 +7,59 @@
 
 import Express from 'express'
 import request from 'supertest'
-import { stub } from 'sinon'
+import { mock, stub } from 'sinon'
 import { expect } from 'chai'
+import 'sinon-mongoose'
+
 import apiRoutes from '../../../src/api'
+import { publicFields } from '../../../src/api/controllers/posts'
+import { Post } from '../../../src/api/models'
+
 
 describe('api/controllers/posts.js', () => {
-  let app, agent 
+  let agent, PostMock
   
-  before((done) => {
-    app = Express()
+  before(() => {
+    const app = Express()
+    
     app.use('/api/v1', apiRoutes)
-    
     agent = request.agent(app)
-    
-    done()
   })
   
-  after((done) => {
-    done()
+  beforeEach(() => {
+    PostMock = mock(Post)
+  })
+  
+  afterEach(() => {
+    PostMock.restore()
   })
   
   describe('getPosts :: by default parameters', () => {
     it('should return list of published post', (done) => {
+      PostMock
+        .expects('find').withArgs({ 
+          publishedAt: { 
+            '$exists': true 
+          } 
+        })
+        .chain('select').withArgs(publicFields.join(' '))
+        .chain('limit').withArgs(5)
+        .chain('skip').withArgs((1 - 1) * 5)
+        .chain('sort').withArgs({
+          publishedAt: 'desc'
+        })
+        .chain('exec')
+        .yields(null)
+        
       agent
-        .get('/api/v1/posts')
-        .expect(200, done)
+        .get('/api/v1/posts?page[number]=1&page[size]=5')
+        .query({
+          'page[number]': 1,
+          'page[size]': 5
+        })
+        .end((err, data) => {
+          done()
+        })
     })
   })
 })
