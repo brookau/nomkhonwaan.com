2505ba7417aba64a29073fd7176456d4154037f0
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-23 15:09:34 +0700

    Fix bug on mongoose model schema

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -10,25 +10,24 @@ import mongoose, { Schema } from 'mongoose'
 /**
  * Post model
  * 
- * Store both post content both of markdown and HTML string. 
- * Special properties tags and users are de-normalized and follow MongoDB guide.
+ * Store post content both of markdown and HTML string, some special properties
+ * are de-normalized follow MongoDB guide
  */
-export default mongoose.model(
-  'Post', 
-  new Schema({
-    createdAt:   { 
-      type: Date, 
-      default: Date.now 
-    },
-    updatedAt:   Date,
-    publishedAt: Date,
-    tags:        [Schema.Types.Mixed],
-    users:       [Schema.Types.Mixed],
-    title:       String,
-    slug:        String,
-    markdown:    String,
-    html:        String
-  }, { 
-    collection: 'posts' 
-  })
-)
+const postSchema = new Schema({
+  createdAt:   { 
+    type: Date, 
+    default: Date.now 
+  },
+  updatedAt:   Date,
+  publishedAt: Date,
+  tags:        [Schema.Types.Mixed],
+  users:       [Schema.Types.Mixed],
+  title:       String,
+  slug:        String,
+  markdown:    String,
+  html:        String
+}, { 
+  collection: 'posts' 
+})
+  
+export default mongoose.model('Post', postSchema)
--- src/api/models/user.js
+++ src/api/models/user.js
@@ -10,16 +10,15 @@ import mongoose, { Schema } from 'mongoose'
 /**
  * User model
  * 
- * Store user profile, the user is post's author and system users.
+ * Store user profile, the user is post's author and system users
  */
-export default mongoose.model(
-  'User', 
-  new Schema({
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: Date,
-    email: String,
-    displayName: String
-  }, {
-    collection: 'users'
-  })
-)
+const userSchema = new Schema({
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: Date,
+  email: String,
+  displayName: String
+}, {
+  collection: 'users'
+})
+
+export default mongoose.model('User', userSchema)
