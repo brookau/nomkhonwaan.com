34b8508099f004ce3e15d6be5349b42b8eb41a74
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-23 08:39:13 +0700

    Change const to pure function, document comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,7 +14,13 @@ export const POST = {
   GET_POSTS_RESPONSE: 'GET_POSTS_RESPONSE'
 }
 
-export const onClickMenuButton = (isExpanded) => {
+/**
+ * Handle on click nav menu button
+ * 
+ * @param isExpanded  Boolean Nav menu status, default is false
+ * @returns Object
+ */
+export function onClickMenuButton(isExpanded) {
   return {
     type: NAV.ON_CLICK_MENU_BUTTON,
     isExpanded: ! isExpanded
