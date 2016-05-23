8210b1c4db31fc8c1393885a8e1fea350f3dba0a
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-23 21:00:46 +0700

    Fix re-create declaration on mongoose model

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -7,27 +7,22 @@
 
 import mongoose, { Schema } from 'mongoose'
 
-/**
- * Post model
- * 
- * Store post content both of markdown and HTML string, some special properties
- * are de-normalized follow MongoDB guide
- */
-const postSchema = new Schema({
-  createdAt:   { 
-    type: Date, 
-    default: Date.now 
-  },
-  updatedAt:   Date,
-  publishedAt: Date,
-  tags:        [Schema.Types.Mixed],
-  users:       [Schema.Types.Mixed],
-  title:       String,
-  slug:        String,
-  markdown:    String,
-  html:        String
-}, { 
-  collection: 'posts' 
-})
-  
-export default mongoose.model('Post', postSchema)
+export default (mongoose.models.Post
+  ? mongoose.model('Post')
+  : mongoose.model('Post', new Schema({
+    createdAt:   { 
+      type: Date, 
+      default: Date.now 
+    },
+    updatedAt:   Date,
+    publishedAt: Date,
+    tags:        [Schema.Types.Mixed],
+    users:       [Schema.Types.Mixed],
+    title:       String,
+    slug:        String,
+    markdown:    String,
+    html:        String
+  }, { 
+    collection: 'posts' 
+  })
+))
--- src/api/models/user.js
+++ src/api/models/user.js
@@ -7,18 +7,14 @@
 
 import mongoose, { Schema } from 'mongoose'
 
-/**
- * User model
- * 
- * Store user profile, the user is post's author and system users
- */
-const userSchema = new Schema({
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: Date,
-  email: String,
-  displayName: String
-}, {
-  collection: 'users'
-})
-
-export default mongoose.model('User', userSchema)
+export default (mongoose.models.User
+  ? mongoose.model('User')
+  : mongoose.model('User', new Schema({
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date,
+    email: String,
+    displayName: String
+  }, {
+    collection: 'users'
+  })
+))
