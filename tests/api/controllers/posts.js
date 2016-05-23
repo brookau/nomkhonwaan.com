7b27a86a2399b555cad302fa2f20a80e5e2207f0
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-23 21:01:00 +0700

    Create posts test

--- tests/api/controllers/posts.js
+++ tests/api/controllers/posts.js
@@ -7,6 +7,8 @@
 
 import Express from 'express'
 import request from 'supertest'
+import { Types } from 'mongoose'
+
 import { mock, stub } from 'sinon'
 import { expect } from 'chai'
 import 'sinon-mongoose'
@@ -17,47 +19,93 @@ import { Post } from '../../../src/api/models'
 
 
 describe('api/controllers/posts.js', () => {
-  let agent, PostMock
+  let agent
+  let PostMock
   
   before(() => {
     const app = Express()
-    
     app.use('/api/v1', apiRoutes)
+    
     agent = request.agent(app)
   })
   
   beforeEach(() => {
     PostMock = mock(Post)
   })
-  
+
   afterEach(() => {
     PostMock.restore()
   })
   
-  describe('getPosts :: by default parameters', () => {
+  describe('getPosts :: default parameters', () => {
     it('should return list of published post', (done) => {
+      const _id = Types.ObjectId()
+      const publishedAt = Date.now()
+      const author = {
+        id: Types.ObjectId(),
+        displayName: 'Natcha Luang - Aroonchai',
+        email: 'me@nomkhonwaan.com'
+      }
+      
       PostMock
-        .expects('find').withArgs({ 
-          publishedAt: { 
-            '$exists': true 
-          } 
-        })
-        .chain('select').withArgs(publicFields.join(' '))
-        .chain('limit').withArgs(5)
-        .chain('skip').withArgs((1 - 1) * 5)
-        .chain('sort').withArgs({
-          publishedAt: 'desc'
-        })
+        .expects('find')
+          .withArgs({ 
+            publishedAt: { 
+              '$exists': true 
+            } 
+          })
+        .chain('select')
+          .withArgs(publicFields.join(' '))
+        .chain('limit')
+          .withArgs(5)
+        .chain('skip')
+          .withArgs((1 - 1) * 5)
+        .chain('sort')
+          .withArgs({
+            publishedAt: 'desc'
+          })
         .chain('exec')
-        .yields(null)
+        .yields(null, [{
+          _id,
+          publishedAt,
+          tags: [{
+            name: 'AngularJS',
+            slug: 'angularjs'
+          }],
+          users: [ author ],
+          slug: '',
+          markdown: '',
+          html: ''
+        }])
         
       agent
-        .get('/api/v1/posts?page[number]=1&page[size]=5')
+        .get('/api/v1/posts')
         .query({
           'page[number]': 1,
           'page[size]': 5
         })
-        .end((err, data) => {
+        .end((err, resp) => {
+          expect(err).to.be.null
+          
+          const [ post ] = resp.body.data
+          expect(post.type).to.equal('posts')
+          expect(post.id).to.equal(_id.toString())
+          expect(post.attributes.publishedAt).to.equal(publishedAt)
+          
+          const [ tag ] = post.attributes.tags
+          expect(tag.name).to.equal('AngularJS')
+          expect(tag.slug).to.equal('angularjs')
+          
+          const [ author ] = post.relationships.author.data
+          expect(author.type).to.equal('users')
+          expect(author.id).to.equal(user.id)
+          
+          const [ user ] = resp.body.included
+          expect(user.type).to.equal('users')
+          expect(user.id).to.equal(user.id)
+          expect(user.attributes.displayName).to.equal(user.displayName)
+          expect(user.attributes.email).to.equal(user.email)
+          
           done()
         })
     })
