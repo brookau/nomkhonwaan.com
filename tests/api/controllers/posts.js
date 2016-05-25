7ac82e0e13f508d5c2c3ceb177e80dbc66837678
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-25 09:53:53 +0700

    Add new test for specific parameters

--- tests/api/controllers/posts.js
+++ tests/api/controllers/posts.js
@@ -76,8 +76,8 @@ describe('api/controllers/posts.js', () => {
     PostMock.restore()
   })
   
-  describe('getPosts :: default parameters', () => {
-    it('should return list of published post', (done) => {
+  describe('getPosts', () => {
+    it('should return list of posts by default parameters', (done) => {
       PostMock
         .expects('find')
           .withArgs({ 
@@ -100,15 +100,66 @@ describe('api/controllers/posts.js', () => {
         
       agent
         .get('/api/v1/posts')
-        .query({
-          'page[number]': 1,
-          'page[size]': 5
-        })
         .end((err, resp) => {
           expect(err).to.be.null
           
           const posts = resp.body.data
           expect(posts.length).to.equal(2)
+          expect(posts[0].type).to.equal('posts')
+          expect(posts[0].id).to.equal(validPostsStub[0]._id.toString())
+          expect(posts[0].attributes.publishedAt).to.equal(validPostsStub[0].publishedAt)
+          
+          const tags = posts[0].attributes.tags
+          expect(tags[0].name).to.equal('AngularJS')
+          expect(tags[0].slug).to.equal('angularjs')
+          
+          const authors = posts[0].relationships.author.data
+          expect(authors[0].type).to.equal('users')
+          expect(authors[0].id).to.equal(validAuthorsStub[0].id.toString())
+          
+          const users = resp.body.included
+          expect(users.length).to.equal(1)
+          expect(users[0].type).to.equal('users')
+          expect(users[0].id).to.equal(validAuthorsStub[0].id.toString())
+          expect(users[0].attributes.displayName).to.equal(validAuthorsStub[0].displayName)
+          expect(users[0].attributes.email).to.equal(validAuthorsStub[0].email)
+          
+          done()
+        })
+    })
+    
+    it('should return list of posts by specific parameters', (done) => {
+      PostMock
+        .expects('find')
+          .withArgs({
+            publishedAt: {
+              '$exists': true
+            }
+          })
+        .chain('select')
+          .withArgs(publicFields.join(' '))
+        .chain('limit')
+          .withArgs(1)
+        .chain('skip')
+          .withArgs((1 - 1) * 1)
+        .chain('sort')
+          .withArgs({
+            publishedAt: 'desc'
+          })
+        .chain('exec')
+        .yields(null, validPostsStub.slice(0, 1))
+      
+      agent
+        .get('/api/v1/posts')
+        .query({
+          'page[number]': 1,
+          'page[size]': 1
+        })
+        .end((err, resp) => {
+          expect(err).to.be.null
+          
+          const posts = resp.body.data
+          expect(posts.length).to.equal(1)
           expect(posts[0].type).to.equal('posts')
           expect(posts[0].id).to.equal(validPostsStub[0]._id.toString())
           expect(posts[0].attributes.publishedAt).to.equal(validPostsStub[0].publishedAt)
