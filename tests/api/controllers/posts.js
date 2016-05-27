731e41b649d9593726d701ec7c0ab52dffe2c270
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-27 21:14:42 +0700

    Fix posts test

--- tests/api/controllers/posts.js
+++ tests/api/controllers/posts.js
@@ -5,6 +5,7 @@
 // @created May 22, 2016
 //
 
+import _ from 'lodash'
 import Express from 'express'
 import request from 'supertest'
 import { Types } from 'mongoose'
@@ -16,8 +17,6 @@ import 'sinon-mongoose'
 import apiRoutes from '../../../src/api'
 import { publicFields } from '../../../src/api/controllers/posts'
 import { Post } from '../../../src/api/models'
-
-import { url } from '../../../src/helpers'
 
 // -- Stub --
 
@@ -31,25 +30,37 @@ const validPostsStub = [{
   _id: Types.ObjectId(),
   publishedAt: Date.now(),
   tags: [{
-    name: 'AngularJS',
-    slug: 'angularjs'
+    name: 'Ant',
+    slug: 'ant'
   }],
-  title: '[Stub] AngularJS',
-  slug: 'stub-angularjs',
-  markdown: 'AngularJS',
-  html: '<p>AngularJS</p>',
+  title: '[Stub] Ant',
+  slug: 'stub-ant',
+  markdown: 'Ant',
+  html: '<p>Ant</p>',
   users: validAuthorsStub
 }, {
   _id: Types.ObjectId(),
   publishedAt: Date.now(),
   tags: [{
-    name: 'Banana',
-    slug: 'banana'
+    name: 'Bird',
+    slug: 'bird'
   }],
-  title: '[Stub] Banana',
-  slug: 'stub-banana',
-  markdown: 'Banana',
-  html: '<p>Banana</p>',
+  title: '[Stub] Bird',
+  slug: 'stub-bird',
+  markdown: 'Bird',
+  html: '<p>Bird</p>',
+  users: validAuthorsStub
+}, {
+  _id: Types.ObjectId(),
+  publishedAt: Date.now(),
+  tags: [{
+    name: 'Cat',
+    slug: 'cat'
+  }],
+  title: '[Stub] Cat',
+  slug: 'stub-cat',
+  markdown: 'Cat',
+  html: '<p>Cat</p>',
   users: validAuthorsStub
 }]
 
@@ -65,9 +76,8 @@ describe('api/controllers/posts.js', () => {
   
   before(() => {
     const app = Express()
-    app.use(url('/api/v1/posts'))
-    app.use('/api/v1', apiRoutes)
     
+    app.use('/api/v1', apiRoutes)
     agent = request.agent(app)
   })
   
@@ -88,7 +98,7 @@ describe('api/controllers/posts.js', () => {
               '$exists': true,
             }
           })
-          .yields(null, 2)
+          .yields(null, 3)
     })
     
     afterEach(() => {
@@ -122,15 +132,15 @@ describe('api/controllers/posts.js', () => {
           expect(err).to.be.null
           
           const meta = resp.body.meta 
-          expect(meta.totalItems).to.equal(2)
+          expect(meta.totalItems).to.equal(3)
           
           const links = resp.body.links 
-          expect(links.self).to.equal('/api/v1/posts?page[number]=1&page[size]=5')
+          expect(links.self).to.match(/(?=.*page\[number\]\=1)(?=.*page\[size\]\=5).*$/)
           expect(links.next).to.be.undefined
           expect(links.previous).to.be.undefined
           
           const posts = resp.body.data
-          expect(posts.length).to.equal(2)
+          expect(posts).to.have.length.of.at.most(5)
           expect(posts[0].type).to.equal('posts')
           expect(posts[0].id).to.equal(validPostsStub[0]._id.toString())
           expect(posts[0].attributes.publishedAt).to.equal(validPostsStub[0].publishedAt)
@@ -144,7 +154,7 @@ describe('api/controllers/posts.js', () => {
           expect(authors[0].id).to.equal(validAuthorsStub[0].id.toString())
           
           const users = resp.body.included
-          expect(users.length).to.equal(1)
+          expect(users).to.have.lengthOf(1)
           expect(users[0].type).to.equal('users')
           expect(users[0].id).to.equal(validAuthorsStub[0].id.toString())
           expect(users[0].attributes.displayName).to.equal(validAuthorsStub[0].displayName)
@@ -173,7 +183,7 @@ describe('api/controllers/posts.js', () => {
             publishedAt: 'desc'
           })
         .chain('exec')
-        .yields(null, validPostsStub.slice(-1))
+        .yields(null, validPostsStub.slice(1, 2))
       
       agent
         .get('/api/v1/posts')
@@ -185,15 +195,15 @@ describe('api/controllers/posts.js', () => {
           expect(err).to.be.null
           
           const meta = resp.body.meta
-          expect(meta.totalItems).to.equal(2)
+          expect(meta.totalItems).to.equal(3)
           
           const links = resp.body.links
-          expect(links.self).to.equal('/api/v1/posts?page[number]=2&page[size]=1')
-          expect(links.next).to.be.undefined
-          expect(links.previous).to.equal('/api/v1/posts?page[number]=1&page[size]=1')
+          expect(links.self).to.match(/(?=.*page\[number\]\=2)(?=.*page\[size\]\=1).*$/)
+          expect(links.next).to.match(/(?=.*page\[number\]\=3)(?=.*page\[size\]\=1).*$/)
+          expect(links.previous).to.match(/(?=.*page\[number\]\=1)(?=.*page\[size\]\=1).*$/)
          
           const posts = resp.body.data
-          expect(posts.length).to.equal(1)
+          expect(posts).to.have.length.of.at.most(1)
           expect(posts[0].type).to.equal('posts')
           expect(posts[0].id).to.equal(validPostsStub[1]._id.toString())
           expect(posts[0].attributes.publishedAt).to.equal(validPostsStub[1].publishedAt)
@@ -207,7 +217,7 @@ describe('api/controllers/posts.js', () => {
           expect(authors[0].id).to.equal(validAuthorsStub[0].id.toString())
           
           const users = resp.body.included
-          expect(users.length).to.equal(1)
+          expect(users).to.have.lengthOf(1)
           expect(users[0].type).to.equal('users')
           expect(users[0].id).to.equal(validAuthorsStub[0].id.toString())
           expect(users[0].attributes.displayName).to.equal(validAuthorsStub[0].displayName)
