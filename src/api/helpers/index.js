e033d3147e76acdd724833505f09c88ebd1f6c35
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-27 20:30:10 +0700

    Add helper index.js

---
+++ src/api/helpers/index.js
@@ -0,0 +1,10 @@
+// api/helpers/index.js
+// --------------------
+//
+// @author  Natcha Luang - Aroonchai <me@nomkhonwaan.com>
+// @created May 27, 2016
+//
+
+import pagination from './pagination'
+
+export { pagination }
