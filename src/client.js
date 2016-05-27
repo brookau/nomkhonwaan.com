2d371c4da37a80c487714c7a192c930e9a56e96d
Natcha LUANGAROONCHAI <natcha.lua@krungthai-axa.co.th>
2016-05-27 22:05:15 +0700

    Add persist state

--- src/client.js
+++ src/client.js
@@ -14,9 +14,10 @@ import { ReduxAsyncConnect } from 'redux-connect'
 import { browserHistory, Router } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
+import { persistState } from 'redux-devtools'
+
 import reducers from './reducers'
 import routes from './routes'
-import middleware from './middleware'
 
 // Grab the state from a global injected into server-generated HTML
 const initialState = window.__INITIAL_STATE__
@@ -26,8 +27,8 @@ const store = createStore(
   reducers,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.devToolsExtension && window.devToolsExtension()
+    window.devToolsExtension && window.devToolsExtension(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
   )
 )
 
